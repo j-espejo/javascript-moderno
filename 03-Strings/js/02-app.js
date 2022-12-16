3dc110bef9525d77f80d1fe48645a428203e8f64
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-16 17:34:52 -0300

    String Methods - includes y length

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,14 @@
+const producto = 'Monitor 24 pulgadas"';
+
+//Metodos String
+console.log(producto);
+
+// propiedad length: Conocer la cantidad de letras de cadena de texto
+console.log(producto.length);
+
+// console.log(producto.indexOf("pulgadas"));
+
+console.log(producto.includes("pulgadas"));
+
+//Revisa por minusculas y mayusculas
+console.log(producto.includes("Pulgadas"));
