091ba3ea3ba683ac4a8ed1caccd84d44b712068c
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-16 17:18:53 -0300

    Todo lo que debes saber de las variables con var

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,44 @@
+//Iniciar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+//Las variables se pueden reasignar
+producto = "Monitor de 19 Pulgadas";
+console.log(producto);
+
+//JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 123;
+console.log(producto);
+
+//Se pueden inicializar sin valor y asignar despues
+var disponible;
+disponible = true;
+
+disponible = false;
+
+console.log("Disponible: " + disponible);
+
+//Inicializar multiples variables
+var categoria = "Computadora",
+  marca = "Marca Famosa",
+  calificacion = 5;
+
+// NO pueden iniciar una variable con un numero
+
+/*
+
+    var 99dias
+    var 01_
+    var 01_
+    var _usuario
+    var __usuario
+*/
+
+// Forma Común
+var nombreProductoCategoriaPrecio;
+
+//underscore
+var nombre_producto;
+
+//Pascalcase
+var NombreProducto;
