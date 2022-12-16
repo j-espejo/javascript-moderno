a8e086e3ad83325964b188b9449ab3a544f44a2d
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-16 17:26:34 -0300

    Todo lo que debes saber de las variables con const

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,11 @@
+const producto = "Monitor 24 pulgadas";
+//Const no puede ser reasignado
+// producto = "Monitor";
+
+console.log(producto);
+
+//const debe inicializar con un valor
+// const precio;
+// precio = 20;
+const precio = 20;
+console.log(precio);
