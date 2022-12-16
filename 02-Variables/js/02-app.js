11d716815312a1518f46060eea7e4eb646c5e206
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-16 17:23:24 -0300

    Todo lo que debes saber de las variables con let

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,18 @@
+let producto = "Tablet";
+
+// Reasignar el valor
+producto = "Monitor";
+
+producto = 20;
+producto = true;
+producto = null;
+
+console.log(producto);
+
+let precio;
+//no tiene nada defino
+console.log(precio);
+
+precio = 300;
+
+console.log(precio);
