f3276443e554deae14dcd54e3091f9ee6e1f763a
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-16 17:05:51 -0300

    Comentar el codigo en JavaScript

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,5 +1,10 @@
+// Pregunta la usuario su nombre
 const nombre = prompt("Cual es tu nombre?");
 
+//Toma nombre y lo muestra en pantalla
 document.querySelector(
   ".contenido"
 ).innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+// Asigna el valor a la variable producto
+const producto = "Monutor 24 pulgadas";
