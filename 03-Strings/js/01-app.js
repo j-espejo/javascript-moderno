e9dcc5dcaa04480862a4e097395b4eaf2cc233ce
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-16 17:30:14 -0300

    Crear String en JavaScript

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,7 @@
+const producto = 'Monitor 24 pulgadas"';
+const producto2 = String("Monitor 20 pulgadas");
+const producto3 = new String("Monitor 19 pulgadas");
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
