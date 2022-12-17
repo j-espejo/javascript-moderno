79c338cf99d5169787393350fff5b7e454585095
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-17 14:55:46 -0300

    Incremento o Decremento

---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,12 @@
+let puntaje = 5;
+
+puntaje += 3; // puntaje = puntaje + 3
+puntaje -= 3;
+
+// puntaje++; // 5
+// ++puntaje; // 6
+
+// puntaje--; // 5
+// --puntaje; // 4
+
+console.log(puntaje);
