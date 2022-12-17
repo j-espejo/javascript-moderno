45707138e5beea0dbe33d91277a71ff864804280
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-17 15:17:04 -0300

    Comparar Null y undefined en JavaScript

---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,14 @@
+let numero;
+
+// si se define sin valor es undefined
+console.log(numero);
+console.log(typeof numero);
+
+//Null
+let numero2 = null;
+console.log(numero2);
+// los null son objetos
+console.log(typeof numero2);
+
+console.log(numero == numero2);
+console.log(numero === numero2);
