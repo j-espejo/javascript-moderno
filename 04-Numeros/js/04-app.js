ba142611afc0c6fb20204af332daf7b429fc7c3e
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-17 14:49:39 -0300

    El orden de las operaciones

---
+++ 04-Numeros/js/04-app.js
@@ -0,0 +1,14 @@
+let resultado;
+
+// Orden en que se ejecutan las funciones
+resultado = 20 + 30 * 2;
+
+resultado = (20 + 30) * 2;
+
+//20% descuento en tu carrito de compra
+
+resultado = (20 + 30 + 40 + 50 + 60) * 0.2;
+
+resultado = (20 + 30) * 1.19;
+
+console.log(resultado);
