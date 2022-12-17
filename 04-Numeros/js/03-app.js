1f44fb3edd4f25feaa288ca615e53e45cda31796
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-17 13:52:20 -0300

    El Objeto Math

---
+++ 04-Numeros/js/03-app.js
@@ -0,0 +1,34 @@
+let resultado;
+
+// PI
+resultado = Math.PI;
+
+// Redondear
+resultado = Math.round(2.4);
+resultado = Math.round(2.5);
+
+// Redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+// Redondear hacia abajo
+resultado = Math.floor(2.9);
+
+// Raiz cuadrada
+resultado = Math.sqrt(144);
+
+// Valor Absoluto
+resultado = Math.abs(-500);
+
+// Potencia
+resultado = Math.pow(2, 4);
+
+// Minimo
+resultado = Math.min(3, 5, 1, -3, 12);
+
+// Maximo
+resultado = Math.max(3, 5, 1, -3, 12);
+
+// Aleatorio dentro de un rango del 1 - 30
+resultado = Math.floor(Math.random() * 30) + 1;
+
+console.log(resultado);
