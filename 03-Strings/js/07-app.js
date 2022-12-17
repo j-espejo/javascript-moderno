44418b01f51d9df1f10423327fdc6c739387278b
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-17 13:36:05 -0300

    String Methods - Convertir a Mayusculas o Minusculas

---
+++ 03-Strings/js/07-app.js
@@ -0,0 +1,15 @@
+const producto = "Monitor 24 pulgadas";
+
+console.log(producto.toUpperCase());
+console.log(producto.toLowerCase());
+
+const email = "CORREO@GMAIL.COM";
+
+console.log(email.toLowerCase());
+
+const precio = 3000;
+
+console.log(precio);
+
+// Number to String
+console.log(precio.toString());
