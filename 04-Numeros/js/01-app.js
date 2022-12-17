c6b2ce63b970f36b9a8515d5cdf476000f4bf488
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-17 13:41:00 -0300

    Creacion Numeros en JavaScript

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,17 @@
+// Creación de números
+
+const numero1 = 30;
+const numero2 = 20;
+const numero3 = 20.2;
+const numero4 = 0.2;
+const numero5 = -20;
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+
+// Objeto con número 80
+const numero6 = new Number(80);
+console.log(numero6);
