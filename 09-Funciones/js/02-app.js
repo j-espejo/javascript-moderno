7a4568d047e50d47908fd5204692b9950af1ffa7
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-19 16:55:52 -0300

    Diferencia entre Funciones expression y declaration

---
+++ 09-Funciones/js/02-app.js
@@ -0,0 +1,13 @@
+// Declaración de Función (Function Declaration)
+sumar();
+
+function sumar() {
+  console.log(2 + 2);
+}
+
+// Expresion de Funcion (Function Expression)
+const sumar2 = function () {
+  console.log(3 + 3);
+};
+
+sumar2();
