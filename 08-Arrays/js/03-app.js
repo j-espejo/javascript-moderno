077b706a0cb851c5f715c737c7f48aec3aa2d494
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-19 11:22:24 -0300

    Recorrer un Array

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,13 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"];
+
+console.table(meses[0]);
+console.table(meses[1]);
+
+// Cuanto mide el arreglo, inicia en 1
+console.log(meses.length);
+
+// Recorrer Arrays
+
+for (let i = 0; i < meses.length; i++) {
+  console.log(meses[i]);
+}
