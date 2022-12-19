b3221017489dbdf372198b9c73591cd14a1f2c1f
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-19 17:07:51 -0300

    Diferencia entre Funciones y Metodos

---
+++ 09-Funciones/js/04-app.js
@@ -0,0 +1,14 @@
+const numero1 = 20;
+const numero2 = "20";
+
+// contexto en q son utilizadas
+
+console.log(parseInt(numero2)); //esto es una funcion con argumento
+
+console.log(numero1.toString()); // es un metodo
+
+function sumar() {
+  console.log(2 + 2);
+}
+
+sumar();
