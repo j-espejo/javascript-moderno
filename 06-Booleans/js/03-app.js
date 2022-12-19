0d492cfb996f4e62e0b799e7990d8de91f02913d
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-19 09:10:58 -0300

    Buenas practicas  a la hora de evaluar Boolean

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = false;
+
+// if (autenticado) {
+//   console.log("Si puedes ver Netflix");
+// } else {
+//   console.log("No puedes verlo");
+// }
+
+// Operador ternario
+
+console.log(autenticado ? "Si esta autenticado" : "No esta autenticado");
