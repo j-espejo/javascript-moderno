f0e14ccf2e5376de2cd06d93790cd42617c3d98e
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-19 17:47:57 -0300

    Parametros y Argumentos en Funciones

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,17 @@
+//a y b son parametros
+function sumar(a, b) {
+  console.log(a + b);
+}
+
+// 2 y 3 son argumentos
+sumar(2, 3);
+
+sumar(200, 184);
+sumar(124, 321);
+
+function saludar(nombre, apellido) {
+  console.log(`Hola, ${nombre} ${apellido}`);
+}
+
+saludar("Jorge", "Espejo");
+saludar("Jorge");
