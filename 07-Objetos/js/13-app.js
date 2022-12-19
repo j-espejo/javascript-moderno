f271b37f2efa7b0b358fe33777da9dd125b0f25a
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-19 10:43:24 -0300

    Object .keys .values .entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,12 @@
+const producto = {
+  nombre: "Monitor 24 Pulgadas",
+  precio: 300,
+  disponible: true,
+};
+// Retorna array con los keys del objeto
+console.log(Object.keys(producto));
+// valores del objeto
+console.log(Object.values(producto));
+
+// retorna en pares de valor
+console.log(Object.entries(producto));
