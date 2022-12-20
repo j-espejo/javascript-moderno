9dd9356b89d269aec959fe9316f925c712d39f63
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-20 10:59:00 -0300

    Comparando Mayor o Menor que

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,10 @@
+// Operador mayor que o menor que
+
+const dinero = 300;
+const totalAPagar = 300;
+
+if (dinero >= totalAPagar) {
+  console.log("Si podemos pagar");
+} else {
+  console.log("Fondos Insuficientes");
+}
