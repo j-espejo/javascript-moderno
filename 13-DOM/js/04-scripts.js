dddd083267e763665cbc17c2f39a2bc289035e05
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-22 12:42:03 -0300

    querySelector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,18 @@
+const card = document.querySelector(".card");
+console.log(card);
+
+const info = document.querySelector(".premium .info");
+console.log(info);
+
+// Celeccionar el segundo card de hospedaje
+const segundoCard = document.querySelector("section.hospedaje .card:nth-child(2)");
+console.log(segundoCard);
+
+// Seleccionar id, selecciona el primero que encuentre
+const formulario = document.querySelector("#formulario");
+console.log(formulario);
+
+// Seleccionar elementos HTML
+
+const navegacion = document.querySelector("nav");
+console.log(navegacion);
