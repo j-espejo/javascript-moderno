1820cf78c8f41f6abc97a9a0a380ecc556fb912f
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-22 14:01:46 -0300

    Cambiando el CSS con JavaScript

---
+++ 13-DOM/js/07-scripts.js
@@ -0,0 +1,11 @@
+// const encabezado = document.querySelector("h1");
+
+// encabezado.style.backgroundColor = "red";
+// encabezado.style.fontFamily = "Arial";
+// encabezado.style.textTransform = "uppercase";
+
+const card = document.querySelector(".card");
+card.classList.add("nueva-clase", "segunda-clase");
+card.classList.remove("segunda-clase");
+
+console.log(card.classList);
