92bb88165dad4b4749c5d6e3ee61053fd615c7c3
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-22 12:32:10 -0300

    getElementById

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,7 @@
+// retorna el primer elemento 'id'
+const formulario = document.getElementById("formulario");
+console.log(formulario);
+
+// retorna null
+// const noExiste = document.getElementById("no-existe");
+// console.log(noExiste);
