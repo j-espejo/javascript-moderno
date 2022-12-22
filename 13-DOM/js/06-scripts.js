f22318486a9d3bff87e1644e4d79f18280aca831
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-22 13:01:52 -0300

    Modificar Textos o Imagenes con JS

---
+++ 13-DOM/js/06-scripts.js
@@ -0,0 +1,12 @@
+const encabezado = document.querySelector(".contenido-hero h1");
+console.log(encabezado);
+
+// console.log(encabezado.innerText); // No encuentra un texto con  visibility: hidden
+// console.log(encabezado.textContent);
+// console.log(encabezado.innerHTML); // se trae el HTML
+
+// const nuevoHeading = "Nuevo Heading";
+// document.querySelector(".contenido-hero h1").textContent = nuevoHeading;
+
+const imagen = document.querySelector(".card img");
+imagen.src = "img/hacer2.jpg";
