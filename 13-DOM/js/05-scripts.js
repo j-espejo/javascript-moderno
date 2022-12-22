cc4f30e2a6c329247384c109aba2501d0164d028
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-22 12:48:19 -0300

    querySelectorAll

---
+++ 13-DOM/js/05-scripts.js
@@ -0,0 +1,9 @@
+// Retorna todos los elementos
+const card = document.querySelectorAll(".card");
+console.log(card);
+
+const formularios = document.querySelectorAll("#formulario");
+console.log(formularios);
+
+const noExiste = document.querySelectorAll(".no-existe");
+console.log(noExiste);
