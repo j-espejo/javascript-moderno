3735cb1852593ecfa1b42ba8b49f12d8695bf9a9
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-26 14:40:19 -0300

    Eventos para Validar formularios

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,4 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+
+  // Asignar eventos
+  inputEmail.addEventListener("blur", function (e) {
+    console.log(e.target.value);
+  });
 });
