fcd5ba0291cd7e71070d1c6a3ed956374facf706
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-26 14:34:49 -0300

    Primeros pasos Proyecto - Simulador de envio de Emails

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,7 @@
+//Ejecuta cuando el documento ha sido cargado
+document.addEventListener("DOMContentLoaded", function () {
+  //Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+});
