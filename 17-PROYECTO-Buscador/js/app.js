e683bdd9fdf5dcb2c861596d90779f355484550d
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-28 10:33:45 -0300

    Filtrar por Precio

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -48,10 +48,14 @@ year.addEventListener("change", function (e) {
 
 minimo.addEventListener("change", function (e) {
   datosBusqueda.minimo = e.target.value;
+
+  filtrarAuto();
 });
 
 maximo.addEventListener("change", function (e) {
   datosBusqueda.maximo = e.target.value;
+
+  filtrarAuto();
 });
 
 puertas.addEventListener("change", function (e) {
@@ -106,7 +110,11 @@ function llenarSelect() {
 //Funcion que filtra en base a la busqueda
 
 function filtrarAuto() {
-  const resultado = autos.filter(filtrarMarca).filter(filtrarYear);
+  const resultado = autos
+    .filter(filtrarMarca)
+    .filter(filtrarYear)
+    .filter(filtrarMinimo)
+    .filter(filtrarMaximo);
 
   // console.log(resultado);
   mostrarAutos(resultado);
@@ -126,5 +134,23 @@ function filtrarYear(auto) {
   if (year) {
     return auto.year === year;
   }
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  const { minimo } = datosBusqueda;
+
+  if (minimo) {
+    return auto.precio >= minimo;
+  }
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  const { maximo } = datosBusqueda;
+  if (maximo) {
+    return auto.precio <= maximo;
+  }
+
   return auto;
 }
