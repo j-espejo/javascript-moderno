b1803c1853ddef1536030092e4c57d211eb1eebc
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-28 13:40:43 -0300

    Validando el Formulario

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -13,5 +13,30 @@ function eventListeners() {
 function agregarTweet(e) {
   e.preventDefault();
 
-  console.log("agregando tweets");
+  // Textarea donde el usuario escribe
+  const tweet = document.querySelector("#tweet").value;
+
+  // validacion
+  if (tweet === "") {
+    mostrarError("Un mensaje no puede ir vacio");
+    return; // evita que se ejecuten mas lineas de codigo
+  }
+
+  console.log("Agregando tweet");
+}
+
+// Mostrar mensaje error
+function mostrarError(error) {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = error;
+  mensajeError.classList.add("error");
+
+  // Insertarlo en el contenido
+  const contenido = document.querySelector("#contenido");
+  contenido.appendChild(mensajeError);
+
+  // Elimina la alerta despues de 1.5s
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 1500);
 }
