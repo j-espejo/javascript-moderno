fcd4af919902ea2853269727346ff738c918c147
Jespejo <jorge.espejo.ibacache@gmail.com>
2022-12-28 09:42:49 -0300

    Leer el valor de la marca del automovil

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,9 +1,67 @@
 //Variables
+
+const marca = document.querySelector("#marca");
+const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+//Contenedor para los resultados
 const resultado = document.querySelector("#resultado");
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+  marca: "",
+  year: "",
+  minimo: "",
+  maximo: "",
+  puertas: "",
+  transmision: "",
+  color: "",
+};
 
 //Eventos
 document.addEventListener("DOMContentLoaded", function () {
-  mostrarAutos();
+  mostrarAutos(); // muestra los autos al cargar
+
+  // llena las opciones de años
+  llenarSelect();
+});
+
+//Event listener para los select de busqueda
+marca.addEventListener("change", function (e) {
+  datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener("change", function (e) {
+  datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener("change", function (e) {
+  datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener("change", function (e) {
+  datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener("change", function (e) {
+  datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener("change", function (e) {
+  datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener("change", function (e) {
+  datosBusqueda.color = e.target.value;
+
+  console.log(datosBusqueda);
 });
 
 // Funciones
@@ -20,4 +78,14 @@ function mostrarAutos() {
     //insertar en el html
     resultado.appendChild(autoHTML);
   });
+}
+
+// Genera los años del select
+function llenarSelect() {
+  for (let i = max; i >= min; i--) {
+    const opcion = document.createElement("option");
+    opcion.value = i;
+    opcion.textContent = i;
+    year.appendChild(opcion);
+  }
 }
