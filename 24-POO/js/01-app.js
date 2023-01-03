df083301189c8dd92a2a5e55e7f60c8ed313615c
Jespejo <jorge.espejo.ibacache@gmail.com>
2023-01-03 09:46:40 -0300

    Metodos, static en las clases

--- 24-POO/js/01-app.js
+++ 24-POO/js/01-app.js
@@ -4,10 +4,21 @@ class Cliente {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+
+  mostrarInformacion() {
+    return `Cliente: ${this.nombre}, tu saldo es de: $${this.saldo}`;
+  }
+
+  static bienvenida() {
+    return `Bienvenido al cajero`;
+  }
 }
 
 const jespejo = new Cliente("jespejo", 400);
+console.log(jespejo.mostrarInformacion());
 console.log(jespejo);
+// Se llama desde la clase
+console.log(Cliente.bienvenida());
 
 //class expresion
 const Cliente2 = class {
@@ -15,7 +26,11 @@ const Cliente2 = class {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+  mostrarInformacion() {
+    return `Cliente: ${this.nombre}, tu saldo es de: $${this.saldo}`;
+  }
 };
 
 const jorge = new Cliente2("jorge", 500);
+console.log(jorge.mostrarInformacion());
 console.log(jorge);
