c0bf3d6d4bec16722ec1d2627ae6c856c5764767
Jespejo <jorge.espejo.ibacache@gmail.com>
2023-01-03 11:52:22 -0300

    Creando e instanciando la clase Presupuesto

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -9,11 +9,24 @@ function eventListeners() {
 }
 
 // Clases
+class Presupuesto {
+  constructor(presupuesto) {
+    this.presupuesto = Number(presupuesto);
+    this.restante = Number(presupuesto);
+    this.gastos = [];
+  }
+}
+
+class UI {}
+
+// Instanciar
+
+const ui = new UI();
+let presupuesto;
 
 // Funciones
 function preguntarPresupuesto() {
   const presupuestoUsuario = prompt("¿Cual es tu presupuesto?");
-  console.log(Number(presupuestoUsuario));
 
   if (
     presupuestoUsuario === "" ||
@@ -23,4 +36,8 @@ function preguntarPresupuesto() {
   ) {
     window.location.reload();
   }
+
+  // Presupuesto valido
+  presupuesto = new Presupuesto(presupuestoUsuario);
+  console.log(presupuesto);
 }
