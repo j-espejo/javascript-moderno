e43a9a492d4e22ae0922cf31df899dbf5eb870c3
Jespejo <jorge.espejo.ibacache@gmail.com>
2023-01-06 11:56:56 -0300

    Iteradores en JavaScript

---
+++ 27-SetsMapsSymbols/js/08-app.js
@@ -0,0 +1,57 @@
+const ciudades = ["Londres", "New York", "Madrid", "Paris"];
+const ordenes = new Set([123, 231, 131, 102]);
+const datos = new Map();
+
+datos.set("nombre", "Jorge");
+datos.set("profesion", "Desarrollador");
+
+// // Entries Iterator: llave y valor
+// for (let entry of ciudades.entries()) {
+//   console.log(entry);
+// }
+
+// for (let entry of ordenes.entries()) {
+//   console.log(entry);
+// }
+
+// for (let entry of datos.entries()) {
+//   console.log(entry);
+// }
+
+// // Values Iterator
+// for (let value of ciudades.values()) {
+//   console.log(value);
+// }
+
+// for (let value of ordenes.values()) {
+//   console.log(value);
+// }
+
+// for (let value of datos.values()) {
+//   console.log(value);
+// }
+
+// // Keys Iterator
+// for (let keys of ciudades.keys()) {
+//   console.log(keys);
+// }
+
+// for (let keys of ordenes.keys()) {
+//   console.log(keys);
+// }
+// for (let keys of datos.keys()) {
+//   console.log(keys);
+// }
+
+// Default
+for (let ciudad of ciudades) {
+  console.log(ciudad);
+}
+
+for (let orden of ordenes) {
+  console.log(orden);
+}
+
+for (let dato of datos) {
+  console.log(dato);
+}
