669b1e883eec843671977983e96c8ecf3546e13a
Jespejo <jorge.espejo.ibacache@gmail.com>
2023-01-09 14:48:40 -0300

    Agregando una cita a la DB

--- 31-PROYECTO-IndexedDB/js/app.js
+++ 31-PROYECTO-IndexedDB/js/app.js
@@ -210,7 +210,7 @@ function nuevaCita(e) {
 
     editando = false;
   } else {
-    // Nuevo Registrando
+    // Nuevo Registro
 
     // Generar un ID único
     citaObj.id = Date.now();
@@ -218,8 +218,21 @@ function nuevaCita(e) {
     // Añade la nueva cita
     administrarCitas.agregarCita({ ...citaObj });
 
-    // Mostrar mensaje de que todo esta bien...
-    ui.imprimirAlerta("Se agregó correctamente");
+    // Insertar Registro en IndexedDB
+    const transaction = DB.transaction(["citas"], "readwrite");
+
+    // Habilitar objectStore
+    const objectStore = transaction.objectStore("citas");
+
+    // Insertar en la DB
+    objectStore.add(citaObj);
+
+    transaction.oncomplete = function () {
+      console.log("Cita agregada");
+
+      // Mostrar mensaje de que todo esta bien...
+      ui.imprimirAlerta("Se agregó correctamente");
+    };
   }
 
   // Imprimir el HTML de citas
