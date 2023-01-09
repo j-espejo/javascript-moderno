2064dea1867cd7bb9ff739483bca022a8be5d2e1
Jespejo <jorge.espejo.ibacache@gmail.com>
2023-01-09 09:53:18 -0300

    Creando las Tablas - IndexedDB

--- 30-IndexedDB/js/app.js
+++ 30-IndexedDB/js/app.js
@@ -17,7 +17,20 @@ function crmDB() {
   };
 
   // configuracion de la base de datos
-  crmDB.onupgradeneeded = function () {
-    console.log("Este metodo solo se ejecuta una vez...");
+  crmDB.onupgradeneeded = function (e) {
+    const db = e.target.result;
+
+    // Object store
+    const objectStore = db.createObjectStore("crm", {
+      keyPath: "crm",
+      autoIncrement: true,
+    });
+
+    // Definir columnas
+    objectStore.createIndex("nombre", "nombre", { unique: false });
+    objectStore.createIndex("email", "email", { unique: true });
+    objectStore.createIndex("telefono", "telefono", { unique: false });
+
+    console.log("Columnas Creadas");
   };
 }
