6153d7fd16f0c2bf071bec67747c853745b40a11
Jespejo <jorge.espejo.ibacache@gmail.com>
2023-01-10 12:28:15 -0300

    Eliminar citas - IndexedDB

--- 31-PROYECTO-IndexedDB/js/app.js
+++ 31-PROYECTO-IndexedDB/js/app.js
@@ -284,9 +284,19 @@ function reiniciarObjeto() {
 }
 
 function eliminarCita(id) {
-  administrarCitas.eliminarCita(id);
+  const transaction = DB.transaction(["citas"], "readwrite");
+  const objectStore = transaction.objectStore("citas");
 
-  ui.imprimirCitas();
+  objectStore.delete(id);
+
+  transaction.oncomplete = () => {
+    console.log(`Cita ${id} eliminada`);
+    ui.imprimirCitas();
+  };
+
+  transaction.onerror = () => {
+    console.log("Hubo un error");
+  };
 }
 
 function cargarEdicion(cita) {
